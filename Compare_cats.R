7ada4d8117264ab76b7407e592d4b3e6584e9341
Pavel Syomin <pas.ntg@gmail.com>
2020-06-01 14:29:18 +0500

    Написана часть обсуждения.

--- Compare_cats.R
+++ Compare_cats.R
@@ -32,9 +32,9 @@ rcats_nodata <- nrow_total - nrow_filled
 # Function to calculate the index of similarity
 # Used in apply
 # Index is between 0 and 1, where 1 is a maximum similarity
-calculate_similarity <- function(x) {
+calculate_similarity <- function(x, table) {
   rcat_number <- as.integer(x[1])
-  rcat_objectives <- as.integer(x[-c(1:4)])
+  rcat_objectives <- as.integer(x[-1])
   raw_result <- apply(table, 1, function(y) {
     catid <- as.integer(y[1])
     cat_name <- y[2]
@@ -52,7 +52,7 @@ calculate_similarity <- function(x) {
 }
 
 # Process data
-x <- as.data.frame(t(apply(data, 1, calculate_similarity)))
+x <- as.data.frame(t(apply(data[-c(2:4)], 1, calculate_similarity, table)))
 colnames(x) <- c("number", "zap", "np", "pp", "zak", "mon", "dpbg")
 # Add categories' names, regions and federal districts by joining with initial datarfame using numbers of regional caterories
 comparison_results <- merge(data[, 1:4], x, by = "number")
@@ -62,7 +62,7 @@ comparison_results[, c("max", "zap_is_max", "np_is_max", "pp_is_max", "zak_is_ma
   max <- max(x)
   fcat_names <- c("zap", "np", "pp", "zak", "mon", "dpbg")
   # In index is lower than 0.7, there is no similar federal category, and the regional category is treated as “original”
-  if (max >= .7) {
+  if (max >= .65) {
     fcat_is_max <- c(as.integer(x == max), 0)
     fcat_max_names <- paste0(fcat_names[x == max], collapse = "+")
   }
@@ -156,3 +156,68 @@ indices_distr_plot <- comparison_results %>%
   geom_density(color = "blue") +
   geom_vline(xintercept = .72, color = "red") +
   facet_wrap(~fcat)
+
+
+# Compare within-group and between-group variability in indices
+# Group 1: federal categories
+# Group 2: regional categories
+# Can we say that within-group similarity of categories (especially federal ones) is smaller than between-groups similatiry?
+# If true, this is a strong point to the conclusion that regional categories are in fact more similar to federal categories that they should be
+fcats_fcats_comparison <- as.data.frame(t(apply(table[-2], 1, calculate_similarity, table)))
+colnames(fcats_fcats_comparison) <- c("number", "zap", "np", "pp", "zak", "mon", "dpbg")
+fcats_fcats_indices <- do.call(c, fcats_fcats_comparison[-1])
+fcats_fcats_indices <-  fcats_fcats_indices[fcats_fcats_indices != 1]
+qplot(fcats_fcats_indices, geom = "bar", xlim = c(0, 1))
+min(fcats_fcats_indices)
+quantile(fcats_fcats_indices, c(.25, .5, .75))
+max(fcats_fcats_indices)
+mean(fcats_fcats_indices)
+shapiro.test(fcats_fcats_indices)
+psych::describe(fcats_fcats_indices)
+fcats_fcats_comparison["max"] <- apply(fcats_fcats_comparison, 1, function(x) {
+  x <- x[-1]
+  x <- x[x != 1]
+  max(x)
+})
+psych::describe(fcats_fcats_comparison["max"])
+
+rcats_fcats_indices <- do.call(c, select(comparison_results, zap:dpbg))
+# rcats_fcats_indices <- rcats_fcats_indices[rcats_fcats_indices != 1]
+qplot(rcats_fcats_indices, geom = "bar", xlim = c(0,1))
+min(rcats_fcats_indices)
+quantile(rcats_fcats_indices, c(.25, .5, .75))
+max(rcats_fcats_indices)
+mean(rcats_fcats_indices)
+shapiro.test(rcats_fcats_indices)
+psych::describe(rcats_fcats_indices)
+psych::describe(comparison_results[comparison_results$max != 1, "max"])
+
+ff_rf_indices_diff <- wilcox.test(fcats_fcats_indices, rcats_fcats_indices, conf.int = TRUE)
+
+rcats_rcats_comparison <- as.data.frame(t(apply(data[-c(2:4)], 1, calculate_similarity, data[-c(3,4)])))
+rcats_rcats_indices <- do.call(c, rcats_rcats_comparison[-1])
+rcats_rcats_indices <- rcats_rcats_indices[rcats_rcats_indices != 1]
+min(rcats_rcats_indices)
+quantile(rcats_rcats_indices, c(.25, .5, .75))
+max(rcats_rcats_indices)
+mean(rcats_rcats_indices)
+# shapiro.test(rcats_rcats_indices) # won't work cause to a sample size limit set to 5000
+qplot(rcats_rcats_indices, geom="bar", xlim = c(0,1))
+rcats_rcats_comparison["max"] <- apply(rcats_rcats_comparison, 1, function(x) {
+  x <- x[-1]
+  x <- x[x != 1]
+  max(x)
+})
+psych::describe(rcats_rcats_comparison["max"])
+
+
+ff_rf_indices_plot <- rbind(
+  data.frame(variant = "ff", indices = fcats_fcats_indices),
+  data.frame(variant = "rf", indices = rcats_fcats_indices)
+) %>% 
+ggplot(aes(x = indices, color = variant)) +
+  geom_density() +
+  geom_density() +
+  scale_color_manual(name = "", labels = c("Внутри федеральных", "Между федеральными и региональными"), values = c(ff = "blue", rf = "red")) +
+  labs(x = "Значение индекса сходства", y = "Плотность") +
+  theme(legend.position = "bottom")
